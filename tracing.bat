1d1aff23481e44d6a01ba5bf1a6a46944ca2b0f5
akashsurwase1 <akash.surwase@partner.bmw.de>
2024-05-24 15:02:42 +0530

    demo example

---
+++ tracing.bat
@@ -0,0 +1,6 @@
+rem This script runs LOBSTER on windows. its genrates the HTML report and runs the CT tool, too.
+lobster-trlc . --config-file="lobster-trlc.conf" --out="trlc.lobster"
+lobster-python .  --out="python.lobster"
+lobster-cpp . --clang-tidy="./helloworld.cpp" --out="cpp.lobster"
+lobster-report --lobster-config="lobster.config" --out="report.lobster"
+lobster-html-report report.lobster --out="tracing.html"
